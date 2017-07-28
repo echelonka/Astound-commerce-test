c95b643fd8c7b96df435fd71490f01b878f83340
echelonka <marinaskripnik97@gmail.com>
2017-07-28 17:05:42 +0300

    test assignment

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
 
 const gulp = require('gulp'),
 	sass = require('gulp-sass'),
+	csso = require('gulp-csso'),
 	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-	return gulp.src('sass/styles.scss')
+	return gulp.src('sass/*.scss')
 		.pipe(sass().on('error', function (error) {
 			console.log(error.formatted);
 			this.emit('end');
@@ -14,11 +15,12 @@ gulp.task('sass', function () {
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
+		.pipe(csso())
 		.pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('sass/**/*.scss', ['sass']);
+	gulp.watch('sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'sass:watch']);
